b48a3005210cc32d981a16605f645974984b0a9a
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-06-18 19:45:48 -0300

    agregamos la configuracion del puerto en la clase Server

---
+++ index.js
@@ -0,0 +1,8 @@
+import Server from "./src/server/config.js";
+
+//instanciar la clase Server
+const server = new Server()
+//agregar las rutas
+
+// escuchar el puerto correspondiente
+server.listen()
