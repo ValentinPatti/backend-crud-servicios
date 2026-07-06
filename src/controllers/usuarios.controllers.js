b4f0188224485f5aca39bce7c3b32d9944095d5b
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-07-06 17:26:27 -0300

    agregue la funcion de listar usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -4,9 +4,20 @@ export const crearUsuario = async (req,res)=>{
     try {
         const usuarioNuevo = new Usuario(req.body)
         await usuarioNuevo.save()
+        res.status(201).json({mensaje: 'El usuario fue creado correctamente'})
     } catch (error) {
-        console.error
+        console.error(error)
         res.status(500).json({mensaje: "Ocurrio un error al intentar crear un usuario"})
+    }
+}
+
+export const listarUsuario = async (req, res) =>{
+    try {
+        const usuarios = await Usuario.find()
+        res.status(201).json(usuarios)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({mensaje: "Ocurrio un error al intentar listar los usuarios"})
     }
 }
 
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { crearUsuario } from "../controllers/usuarios.controllers.js";
+import { crearUsuario, listarUsuario } from "../controllers/usuarios.controllers.js";
 
 const router = Router()
 
-router.route('/').post(crearUsuario)
+router.route('/').post(crearUsuario).get(listarUsuario)
 
 export default router
