a60d2635cc46d2152b5a23f2a0916e15d9a7f513
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-07-06 20:56:15 -0300

    dividimos las reglas de validacion para el post y put, y creamos una alternativa para el patch

--- src/middlewares/validacionServicio.js
+++ src/middlewares/validacionServicio.js
@@ -2,42 +2,32 @@ import { body, param } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 
 
-export const validacionServicio = [
+const reglasServicio = [
   body("nombreServicio")
-    .notEmpty()
-    .withMessage("El nombre del servicio es un campo obligatorio")
     .isString()
     .withMessage('El nombre del servicio debe ser un string')
     .isLength({min:5 ,max:100})
     .withMessage('El nombre del servicio debe tener entre 5 y 100 caracteres')
     ,
     body('precio')
-    .notEmpty()
-    .withMessage("El precio del servicio es un campo obligatorio")
     .isNumeric()
     .withMessage('El precio debe ser en fomato numerico')
     .isFloat({min: 50})
     .withMessage('El precio no puede ser menor a $50')
     ,
     body('descripcion')
-    .notEmpty()
-    .withMessage("La descripcion del servicio es un campo obligatorio")
     .isString()
     .withMessage('La descripcion del servicio debe ser un string')
     .isLength({min:10 ,max:500})
     .withMessage('La descripcion del servicio debe tener entre 5 y 100 caracteres')
     ,
     body('imagen')
-    .notEmpty()
-    .withMessage('La imagen es un dato obligatorio')
     .isString()
     .withMessage('La imagen del servicio debe ser un string')
     .matches(/^https:\/\/.+\.(jpg|jpeg|png|webp|avif|svg)$/)
     .withMessage('La imagen debe ser una url valida y debe terminar con jpg|jpeg|png|webp|avif|svg')
     ,
     body('categoria')
-    .notEmpty()
-    .withMessage('La categoria es un dato obligatorio')
     .isString()
     .withMessage('La categoria del servicio debe ser un string')
     .isIn(["Desarrollo Web", "Backend & API", "Consultoria"])
@@ -45,6 +35,17 @@ export const validacionServicio = [
     ,
     resultadoValidacion
 ];
+
+//para el post y el put
+export const validacionServicio = [
+  ...reglasServicio.map((regla)=> regla.notEmpty().withMessage('El campo es un dato obligatorio')), resultadoValidacion
+]
+
+//para el patch
+export const validacionPatchServicio = [
+  ...reglasServicio.map((regla)=> regla.optional({values: "falsy"})), resultadoValidacion
+]
+
 export const validacionIDServicio = [
     param('id').isMongoId().withMessage('El id enviado no tiene el formato de ID de MongoDB'),
     resultadoValidacion
