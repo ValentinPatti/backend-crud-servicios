41c36f9b17b58bbe6772a2d6b65769296b65cf36
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-07-08 17:55:48 -0300

    agregamos el usuarioSchema para crear usuarios

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -1,33 +1,52 @@
 import mongoose, { Schema } from "mongoose";
 
 const usuarioSchema = new Schema(
-    {
-        nombreUsuario: {
-            type: String,
-            required: true,
-            minLenght: 4,
-            maxLenght: 20
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        rol:{
-            type: String,
-            required: true,
-            enum: ['admin', 'cliente']
-        }
+  {
+    nombreUsuario: {
+      type: String,
+      required: true,
+      minLenght: 3,
+      maxLenght: 100,
+      trim: true,
     },
-    {
-        timestamps: true
-    }
-)
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator: (valor) => {
+          return /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/.test(
+            valor,
+          );
+        },
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate: {
+        validator: (valor) => {
+          return /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,}$/.test(
+            valor,
+          );
+        },
+      },
+    },
+    rol: {
+      type: String,
+      required: true,
+      enum: ["admin", "cliente"],
+      default: "cliente",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const Usuario = mongoose.model('usuario', usuarioSchema)
+const Usuario = mongoose.model("usuario", usuarioSchema);
 
-export default Usuario
+export default Usuario;
